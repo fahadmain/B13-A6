2479e6328e942ff09e3b50bd3927d89cde54ecc7
Fahad Main <fahadmain23@gmail.com>
2026-04-07 15:03:39 +0600

    card design done

---
+++ src/component/ModelCard.jsx
@@ -0,0 +1,27 @@
+import { Check } from "lucide-react";
+import { useState } from "react";
+
+
+const ModelCard = ({model}) => {
+    const [isAdded, setIsAdded] = useState(false)
+    const handleAdded = () =>{
+        setIsAdded(true)
+    }
+    return (
+        <div className='p-5 border border-gray-300 rounded-xl shadow-2xl'>
+            <div className='mb-5 font-bold flex flex-row justify-end'><p className=' bg-[#fef3c6FF] text-yellow-600 w-25 text-center rounded-xl p-1'>{model.tag}</p></div>
+            <img src={model.image} alt="" />
+            <h3 className='text-2xl font-bold mt-5'>{model.name}</h3>
+            <p className='text-gray-500 mt-5'>{model.description}</p>
+            <p className='mt-3'><span className='font-bold text-xl'>${model.price}</span><span className='text-gray-500 text-sm'>/{model.period}</span></p>
+            <div className='mt-5'>
+                <p className='flex gap-1 text-green-500'><Check></Check><span className='text-gray-500'>{model.features1}</span></p>
+                <p className='flex gap-1 text-green-500'><Check></Check><span className='text-gray-500'>{model.features2}</span></p>
+                <p className='flex gap-1 text-green-500'><Check></Check><span className='text-gray-500'>{model.features3}</span></p>
+            </div>
+            <button onClick={handleAdded} className='bg-linear-to-r from-indigo-600 to-purple-600 text-white font-bold rounded-full btn w-full mt-7'>{isAdded ? "Added to Cart" : "Buy Now"}</button>
+        </div>
+    );
+};
+
+export default ModelCard;
--- src/component/Models.jsx
+++ src/component/Models.jsx
@@ -1,13 +1,24 @@
-import React, { use } from 'react';
 
-const Models = ({modelPromise}) => {
+import React, { use} from 'react';
+import ModelCard from './ModelCard';
+
+const Models = ({ modelPromise }) => {
     const models = use(modelPromise)
-    console.log(models)
+    
 
 
     return (
-        <div>
-            
+        <div className='w-11/12 mx-auto mt-16'>
+            <div className='text-center'>
+                <h2 className='text-3xl font-bold'>Premium Digital Tools</h2>
+                <p className='text-gray-500 my-5'>Choose from our curated collection of premium digital products designed <br />
+                    to boost your productivity and creativity.</p>
+            </div>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center'>
+                {models.map(model =>
+                    <ModelCard model={model}></ModelCard>
+                    )}
+            </div>
         </div>
     );
 };
